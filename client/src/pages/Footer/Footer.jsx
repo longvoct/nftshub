0c0408250997a91c8516af117e46a7bd6512ce81
longvoct <volonglqd.com>
2022-06-15 17:35:46 +0700

    Update code

--- client/src/pages/Footer/Footer.jsx
+++ client/src/pages/Footer/Footer.jsx
@@ -59,6 +59,84 @@ function Footer() {
           </div>
         </div>
       </div>
+
+      <div className="footer-desc">
+        <div className="footer-desc__line"></div>
+        <div className="footer-desc__container">
+          <div className="footer-desc__left">
+            <span>NFTs Hubs</span>
+            <p>
+              The digital marketplace for crypto collectibles and non-fungible
+              tokens (NFTs). Buy, sell, and discover exclusive digital items.
+            </p>
+          </div>
+          <div className="footer-desc__right">
+            <div className="footer-desc__menu">
+              <span>Marketplace</span>
+              <ul className="footer-desc__list">
+                <li className="footer-desc__item">All NFTs</li>
+                <li className="footer-desc__item">Solana NFTs</li>
+                <li className="footer-desc__item">Art</li>
+                <li className="footer-desc__item">Collectibles</li>
+                <li className="footer-desc__item">Domain Names</li>
+                <li className="footer-desc__item">Music</li>
+                <li className="footer-desc__item">Photography</li>
+                <li className="footer-desc__item">Sports</li>
+                <li className="footer-desc__item">Trading Cards</li>
+                <li className="footer-desc__item">Utility</li>
+                <li className="footer-desc__item">Virtual Worlds</li>
+              </ul>
+            </div>
+
+            <div className="footer-desc__menu">
+              <span>My Account</span>
+              <ul className="footer-desc__list">
+                <li className="footer-desc__item">Profile</li>
+                <li className="footer-desc__item">Favorites</li>
+                <li className="footer-desc__item">Watchlist</li>
+                <li className="footer-desc__item">My Collections</li>
+                <li className="footer-desc__item">Settings</li>
+              </ul>
+            </div>
+
+            <div className="footer-desc__menu">
+              <span>Resources</span>
+              <ul className="footer-desc__list">
+                <li className="footer-desc__item">Help Center</li>
+                <li className="footer-desc__item">Platform Status</li>
+                <li className="footer-desc__item">Partners</li>
+                <li className="footer-desc__item">Gas-Free Marketplace</li>
+                <li className="footer-desc__item">Taxes</li>
+                <li className="footer-desc__item">Blog</li>
+                <li className="footer-desc__item">Docs</li>
+                <li className="footer-desc__item">Newsletter</li>
+              </ul>
+            </div>
+
+            <div className="footer-desc__menu">
+              <span>Company</span>
+              <ul className="footer-desc__list">
+                <li className="footer-desc__item">About</li>
+                <li className="footer-desc__item">Careers</li>
+                <li className="footer-desc__item">Ventures</li>
+                <li className="footer-desc__item">Grants</li>
+              </ul>
+            </div>
+            <div className="footer-desc__menu">
+              <span>Stats</span>
+              <ul className="footer-desc__list">
+                <li className="footer-desc__item">Rankings</li>
+                <li className="footer-desc__item">Activity</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="footer-desc__line"></div>
+        <div className="footer-desc__policy">
+          <span>Privacy Policy</span>
+          <span>Terms of Service</span>
+        </div>
+      </div>
     </div>
   );
 }
